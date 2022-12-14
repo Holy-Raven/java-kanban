78a0e5283a8223f6da4e90ed6cceb919baac4e5f
Elena Danlina <eleorex@yandex.ru>
2022-12-15 00:09:19 +0300

    refactor: Методы lod, remove и update разбиты на самостоятельные методы

--- src/Main.java
+++ src/Main.java
@@ -6,53 +6,65 @@ import tracker.util.Status;
 
 public class Main {
 
-    // интерфейс HasId я удалила, я добавила его в самом начале выполнения работы. Потому что таким образом нам показывали
-    // в пятничном вебинаре. Думала, что в дальнейшем выполнении спринтов, он будет чем-то "обрастать". Но если нет, то нет)
-    //  в классе Epic я использовала мапу, а не лист, потому что мне показалось удобным везде обращаться к объектам списка
-    // одним и тем же образом, через ключ в виде id. В классе подзадач я хранила ссылку на родителя, а не его id, потому
-    // что у меня был выбор и мне показалось это удобным)
-    // В main не поняла комментарии по поводу логов. Добавила побольше выводов в консоль. Хотя пользователь этого не увидит.
-
     public static void main(String[] args) {
 
         Manager manager = new Manager(1);
 
         System.out.println("Cоздание отдельных задач");
 
-        Task firstTask = new Task("Закрыть ипотеку", "Выплатить всю сумму задолжности",
-                manager.getId(), Status.NEW);
+        Task firstTask = new Task("Закрыть ипотеку",
+                "Выплатить всю сумму задолженности",
+                manager.getId(),
+                Status.NEW);
         manager.loadTask(firstTask);
-        Task secondTask = new Task("Купить кошку", "Подобрать подходящий экземпляр и приобрести",
-                manager.getId(), Status.NEW);
+
+        Task secondTask = new Task("Купить кошку",
+                "Подобрать подходящий экземпляр и приобрести",
+                manager.getId(),
+                Status.NEW);
         manager.loadTask(secondTask);
 
         System.out.println("Создание эпика и две подзадачи в нем");
 
-        Epic firstEpicTask = new Epic("Купить Mustang",  "Shelby GT 500 black edition", manager.getId());
-        manager.loadTask(firstEpicTask);
+        Epic firstEpicTask = new Epic("Купить Mustang",
+                "Shelby GT 500 black edition",
+                manager.getId());
+        manager.loadEpicTask(firstEpicTask);
 
-        SubTask firstStep = new SubTask("Стать IT специалистом", "Закончить курс Яндекс практикума," +
-                " познать тайны JAVA", manager.getId(), Status.IN_PROGRESS, firstEpicTask.getId());
-        manager.loadTask(firstStep);
+        SubTask firstStep = new SubTask("Стать IT специалистом",
+                "Закончить курс Яндекс практикума, познать тайны JAVA",
+                manager.getId(),
+                Status.DONE,
+                firstEpicTask.getId());
+        manager.loadSubTask(firstStep);
+
         SubTask secondStep = new SubTask("Устроиться на работу",
-                "Получать заработную плату от 150 000 руб.", manager.getId(),
-                Status.DONE, firstEpicTask.getId());
-        manager.loadTask(secondStep);
+                "Получать заработную плату от 150 000 руб.",
+                manager.getId(),
+                Status.DONE,
+                firstEpicTask.getId());
+        manager.loadSubTask(secondStep);
 
         System.out.println("Создание эпика и одной подзадачи в нем");
 
         Epic secondEpicTask = new Epic("Отдых за границей",
-                "Посетить остров мечты - Бали", manager.getId());
-        manager.loadTask(secondEpicTask);
+                "Посетить остров мечты - Бали",
+                manager.getId());
+        manager.loadEpicTask(secondEpicTask);
 
-        SubTask thirdStep = new SubTask("Оформить документы", "Необходимо получить загранпаспорт",
-                manager.getId(), Status.NEW, secondEpicTask.getId());
-        manager.loadTask(thirdStep);
+        SubTask thirdStep = new SubTask("Оформить документы",
+                "Необходимо получить загранпаспорт",
+                manager.getId(),
+                Status.NEW,
+                secondEpicTask.getId());
+        manager.loadSubTask(thirdStep);
 
         System.out.println("Удаляем задачи с 1 по 3");
+
         manager.removeTask(1);
         manager.removeTask(2);
-        manager.removeTask(3);
+        manager.removeEpicTask(3);
+        manager.removeSubTask(7);
 
         System.out.println("Выводим полный список задач");
         for (Task task: manager.getTaskMap().values()) {
--- src/tracker/service/Manager.java
+++ src/tracker/service/Manager.java
@@ -3,7 +3,6 @@ package tracker.service;
 import tracker.model.Epic;
 import tracker.model.SubTask;
 import tracker.model.Task;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -23,34 +22,49 @@ public class Manager {
     // Список всех подзадач.
     private final HashMap <Integer, SubTask> subTaskMap = new HashMap<>();
 
-    // На вход метода подается задача, если там не null, то начинаем выполннение нашего алгоритма. Делаем проверку
-    // класса нашей задачи. Проверяем это задача класса Эпик? Если да, то загружаем ее в общий список и список эпик,
-    // обновляем ее статус. Если задача класса Субтаск, то вносим айди задачи в список родительского эпика после этого
-    // загружаем в общий список задач и подзадач. Тут обновляем статус родителя Субтаска. Если задача и не Эпик и не
-    // Субтакс, значит она класса Таск, записываем ее в общий список задач. Если на вход пришел null, о выводим
-    // пользователю, что задача не найдена.
+    // На вход метода подается задача, если там не null, то загружаем ее в общий список задач. Если на вход пришел
+    // null, то выводим пользователю, что задача не найдена.
 
     public void loadTask(Task task) {
 
         if (task != null) {
-            if (task instanceof Epic epic) {
-                taskMap.put(epic.getId(), epic);
-                epicMap.put(epic.getId(), epic);
-                instalStatusEpic(epic);
-            } else if (task instanceof SubTask subTask) {
-
-                epicMap.get(subTask.getEpic()).getSubTaskList().add(subTask.getId());
-                taskMap.put(subTask.getId(), subTask);
-                subTaskMap.put(subTask.getId(), subTask);
-                instalStatusEpic(epicMap.get(subTask.getEpic()));
-
-            } else {
-                taskMap.put(task.getId(), task);
-            }
+            taskMap.put(task.getId(), task);
         } else {
             System.out.println("Сбой, задача не найдена.");
         }
     }
+
+    // На вход метода подается Эпик задача, если там не null, то загружаем ее в общий список задач и список эпиков.
+    // обновляем ее статус. Если на вход пришел null, о выводим пользователю, что задача не найдена.
+
+    public void loadEpicTask(Epic epic) {
+
+        if (epic != null) {
+            taskMap.put(epic.getId(), epic);
+            epicMap.put(epic.getId(), epic);
+            instalStatusEpic(epic);
+        } else {
+            System.out.println("Сбой, задача не найдена.");
+        }
+    }
+
+    // На вход метода подается СубТаск, если там не null, вносим айди задачи в список родительского эпика после
+    // этого загружаем ее в общий список задач и список подзадач. Обновляем статус родителя. Если на вход пришел
+    // null, о выводим пользователю, что задача не найдена.
+
+    public void loadSubTask(SubTask subTask) {
+
+        if (subTask != null) {
+            epicMap.get(subTask.getEpic()).getSubTaskList().add(subTask.getId());
+            taskMap.put(subTask.getId(), subTask);
+            subTaskMap.put(subTask.getId(), subTask);
+            instalStatusEpic(epicMap.get(subTask.getEpic()));
+        } else {
+            System.out.println("Сбой, задача не найдена.");
+        }
+
+    }
+
     // Получение списка всех задач.
     public HashMap<Integer, Task> getTaskMap() { return taskMap; }
 
@@ -120,59 +134,105 @@ public class Manager {
         }
     }
 
-    // Обновление задачи
-    // Сперва по ключу входящей задачи мы проверяем есть ли в общем списке задач такая задача, если находим ее там то
-    // обновляем. Мы нашли задачу в общем списке, значит она может быть и в списке эпиков или списке подзадач. Сперва
-    // проверяем является ли входящая задача эпиком, если да, то значит она есть в списке эпиков, обновляем ее там.
-    // Если задача не является эпиком, то мы идем дальше и проверяем является ли задача подзадачей, если да, то меняем
     // подзадачу в списке подзадач, после этого обновляем статус Эпика-родителя. Если в общем списке нет указанной
     // задачи то, говорим пользователю, что такой задачи нет, заменить не выйдет.
 
+
+    // Сперва по ключу входящей задачи мы проверяем есть ли в общем списке задач такая задача, и есть ли она вообще -
+    // не пришел ли на вход метода null, если находим ее там, то обновляем в общем списке. Если на вход пришел null,
+    // то выводим пользователю, что задача не найдена.
+
     public void updateTask (Task task) {
-        if (taskMap.containsKey(task.getId())) {
+
+        if (task != null && taskMap.containsKey(task.getId())) {
             taskMap.put(task.getId(), task);
-            if (task instanceof Epic epic) {
-                epicMap.put(epic.getId(), epic);
-                instalStatusEpic(epic);
-            }
-            if (task instanceof SubTask subTask) {
-                subTaskMap.put(subTask.getId(), subTask);
-                instalStatusEpic(epicMap.get(subTask.getEpic()));
-            } else {
-                System.out.println("Такой задачи не стоит");
-            }
+        } else {
+            System.out.println("Сбой, задача не найдена.");
         }
     }
 
+    // Сперва по ключу входящей задачи мы проверяем есть ли в общем списке задач такая задача, и есть ли она вообще -
+    // не пришел ли на вход метода null, если находим ее там то обновляем в общем списке и списке эпиков. После этого
+    // обновляем и статус задачи. Если на вход пришел null, то выводим пользователю, что задача не найдена.
+
+
+    public void updateTask (Epic epic) {
+
+        if (epic != null && taskMap.containsKey(epic.getId())) {
+            taskMap.put(epic.getId(), epic);
+            epicMap.put(epic.getId(), epic);
+            instalStatusEpic(epic);
+        } else {
+            System.out.println("Сбой, задача не найдена.");
+        }
+    }
+
+    // Сперва по ключу входящей задачи мы проверяем есть ли в общем списке задач такая задача, и есть ли она вообще -
+    // не пришел ли на вход метода null, если находим ее там то обновляем подзадачу в общем списке и списке подзадач,
+    // после этого обновляем статус Эпика-родителя. Если на вход пришел null, то выводим пользователю, что задача
+    // не найдена.
+
+    public void updateSubTask (SubTask subTask) {
+
+        if (subTask != null && taskMap.containsKey(subTask.getId())) {
+            taskMap.put(subTask.getId(), subTask);
+            subTaskMap.put(subTask.getId(), subTask);
+            instalStatusEpic(epicMap.get(subTask.getEpic()));
+        } else {
+            System.out.println("Сбой, задача не найдена.");
+        }
+    }
+
+
     // Удаление задачи по ID
-    // Проверяем, есть ли задача с таким ID в общем списке задач. Тут же проверяем есть ли задача в списке эпиков, если
-    // находим, то мы должны удалить все ее подзадачи в общем списке и списке подзадач, после этого удаляем и сам эпик
-    // из списка эпиков. Мы можем не найти задачу в списке эпиков, тогда запускаем проверку в списке подзадач, если
-    // находим, то берем эту подзадачу и удаляем ее в списке подзадач, обновляем статус эпика. Если в общем списке нет
-    // задачи с таким ID то, говорим пользователю, что такой задачи нет, удалить ничего не выйдет.
+    // Проверяем, есть ли задача с таким ID в общем списке задач если находим, то удаляем. Если в списке нет задачи с
+    // таким ID то, говорим пользователю, что такой задачи нет, удалить ничего не выйдет.
 
     public void removeTask(int id) {
         if (taskMap.containsKey(id)) {
+            taskMap.remove(id);
+        } else {
+            System.out.println("Сбой, задача не найдена.");
+        }
+    }
 
-            if (epicMap.containsKey(id)) {
-                for (Integer idSubTask : epicMap.get(id).getSubTaskList()) {
-                    subTaskMap.remove(idSubTask);
-                    taskMap.remove(idSubTask);
-                }
-                epicMap.remove(id);
-            }
+    // Проверяем есть ли задача в списке эпиков, если находим, то мы должны удалить все ее подзадачи в общем списке и
+    // списке подзадач, после этого удаляем и сам эпик из списка эпиков и списке задач. Если в списке нет задачи с таким
+    // ID то, говорим пользователю, что такой задачи нет, удалить ничего не выйдет.
 
-            if (subTaskMap.containsKey(id)) {
-                subTaskMap.remove(id);
-                instalStatusEpic(epicMap.get(subTaskMap.get(id).getEpic()));
-            }
+    public void removeEpicTask(int id) {
 
-            if (!epicMap.containsKey(id) && !subTaskMap.containsKey(id)){
-                taskMap.remove(id);
+        if (epicMap.containsKey(id)) {
+            for (Integer idSubTask : epicMap.get(id).getSubTaskList()) {
+                subTaskMap.remove(idSubTask);
+                taskMap.remove(idSubTask);
             }
+            taskMap.remove(id);
+            epicMap.remove(id);
 
         } else {
-            System.out.println("Такой задачи не стоит");
+            System.out.println("Сбой, задача не найдена.");
+        }
+    }
+
+    // Проверяем есть ли задача в списке подзадач, если находим, то записываем id эпика родителя в отдельную переменную,
+    // после этого удаляем id подзадачи из внутреннего списка id подзадач в эпике родителе. Удаляем подзадачу их общего
+    // списка и списка подзадач. Обновляем статус эпика родителя. Если в списке нет задачи с таким ID то, говорим
+    // пользователю, что такой задачи нет, удалить ничего не выйдет.
+
+    public void removeSubTask(Integer id) {
+
+        if (subTaskMap.containsKey(id)) {
+
+            Integer idEpicParent = subTaskMap.get(id).getEpic();
+            epicMap.get(idEpicParent).getSubTaskList().remove(id);
+
+            taskMap.remove(id);
+            subTaskMap.remove(id);
+
+            instalStatusEpic(epicMap.get(idEpicParent));
+        } else {
+            System.out.println("Сбой, задача не найдена.");
         }
     }
 
